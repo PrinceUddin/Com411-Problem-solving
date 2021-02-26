e0e703f80867c6decf4a7cbdac77b12daba32bb2
repl.it user <replituser@example.com>
2021-02-26 11:30:27 +0000

    message

---
+++ basics/output/escape_characters.py
@@ -0,0 +1,7 @@
+#Displaying escape characters
+
+print("\n Displays a new line")
+print("\t Display a teb space")
+print("\\ if i need to preint a backslash")
+print("\" will display double quot")
+print("\' display single quste")
